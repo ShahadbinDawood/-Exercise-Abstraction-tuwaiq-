f64ba977e3c2a92f5f3d66d573f3e0ca180097e5
ShahadbinDawood <shahadbindawood@gmail.com>
2026-04-15 14:04:16 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-    //q1
+        //q1
         Movie movie=new Movie("boo",40,"james");
-        System.out.println("the movie discount is "+movie.getDiscount());
+        System.out.println(movie.getName()+" discount is "+movie.getDiscount()+"\nthe directer is "+movie.getDirector());
         Book book = new Book("let go ",80,"gigi");
-        System.out.println(book.getName()+" discounted price is "+book.getDiscount());
+        System.out.println(book.getName()+" discounted price is "+book.getDiscount()+"\nthe author is "+book.getAuthor());
         //q2
         MovablePoint movablePoint = new MovablePoint(39,20,7,9);
         movablePoint.moveUp();
         movablePoint.moveRight();
+        System.out.println("x  "+movablePoint.getX() + " y  "+movablePoint.getY());
+        movablePoint.moveDawn();
+        movablePoint.moveLeft();
+        movablePoint.moveLeft();
         System.out.println("x  "+movablePoint.getX() + " y  "+movablePoint.getY());
 
     }
